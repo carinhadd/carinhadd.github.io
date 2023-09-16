611cacbd07c252880d1a047586f75c57db7a7ebf
vina <carinhadiscord@gmail.com>
2023-09-16 10:33:59 -0300

    vamos ver o que da daqui

---
+++ src/js.js
@@ -0,0 +1,11 @@
+
+function fecharCoisa(){
+    document.getElementById("lateral").style.width="0";
+    document.getElementById("lateral").style.overflowX="hidden";
+}
+
+function abrirCoisa(){
+	document.getElementById("lateral").style.width="350px";
+    document.getElementById("lateral").style.height="780px";
+    document.getElementById("lateral").style.overflowY='hidden';
+}
