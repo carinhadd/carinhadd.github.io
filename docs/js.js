09f8227c0606fa05a6fbbd7f18a96e48ea50514d
vina <carinhadiscord@gmail.com>
2023-09-17 22:41:10 -0300

    mais mudanças

--- docs/js.js
+++ docs/js.js
@@ -5,7 +5,15 @@ function fecharCoisa(){
 }
 
 function abrirCoisa(){
-	document.getElementById("lateral").style.width="350px";
+    document.getElementById("lateral").style.width="350px";
     document.getElementById("lateral").style.height="780px";
-    document.getElementById("lateral").style.overflowY='hidden';
+}
+function apoie(){
+    document.getElementById("apoieMenu").style.width='400px';
+    document.getElementById("apoieMenu").style.height='400px';
+}
+function fecharApoio(){
+    document.getElementById("apoieMenu").style.width='0';
+    document.getElementById("apoieMenu").style.height='0';
+    document.getElementById("apoieMenu").style.overflowX='hidden';
 }
